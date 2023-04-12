99dc358e80f5e8f4669aec18d0ac07976023cac9
ganeshrevadi <ganeshrevadi16@gmail.com>
2023-04-12 16:15:57 +0530

    Initial Commit

---
+++ controllers/contactController.js
@@ -0,0 +1,45 @@
+const asyncHandler = require("express-async-handler");
+
+//@desc Get all contacts
+//@route GET /api/contacts
+//@access Public
+const getContacts =asyncHandler(async (req , res) => { 
+    res.status(200).json({message :"Get all contacts"});
+});
+
+//@desc Create New contacts
+//@route POST /api/contacts
+//@access Public
+const createContact = asyncHandler( async (req , res) => { 
+    console.log("The request body is :" , req.body);
+    const { name , email , phone } = req.body;
+    if(!name || !email || !phone){
+        res.status(400)
+        throw new Error("Please enter all fields");
+    }
+    res.status(201).json({message :"Create Contact"});
+});
+
+//@desc Get all contacts
+//@route GET /api/contacts/:id
+//@access Public
+const getContact = asyncHandler( async (req , res) => { 
+    res.status(200).json({message: `Get contact for ${req.params.id}`});
+});
+
+//@desc Update all contacts
+//@route PUT /api/contacts/:id
+//@access Public
+const updateContact = asyncHandler( async (req , res) => { 
+    res.status(200).json({message: `Update contact for ${req.params.id}`});
+});
+
+//@desc Delete all contacts
+//@route DELETE /api/contacts/:id
+//@access Public
+const deleteContact = asyncHandler( async (req , res) => { 
+    res.status(201).json({message: `Delete contact for ${req.params.id}`});
+});
+
+
+module.exports = { getContacts , createContact , getContact , updateContact , deleteContact };
---
+++ middleware/errorhandler.js
@@ -0,0 +1,29 @@
+const {constants} = require('../constants');
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode?res.statusCode : 500;
+    res.json({title: "Not Found",message : err.message, stackTrace: err.stack});
+    switch(statusCode){
+        case constants.NOT_FOUND:
+            res.json({title: "Not Found",message : err.message, stackTrace: err.stack});
+            break;
+        case constants.VALIDATION_ERROR:
+            res.json({title: "Validation Failed",message : err.message, stackTrace: err.stack});
+            break;
+        case constants.UNAUTHORIZED:
+            res.json({title: "unauthourized",message : err.message, stackTrace: err.stack});
+            break;
+        case constants.FORBIDDEN:
+            res.json({title: "forbidden",message : err.message, stackTrace: err.stack});
+            break;
+        case constants.SERVER_ERROR:
+            res.json({title: "forbidden",message : err.message, stackTrace: err.stack});
+            break;
+        default:
+            console.log("No errors :-)")
+    
+
+    }
+
+}
+
+module.exports = errorHandler;
